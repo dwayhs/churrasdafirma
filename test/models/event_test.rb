9b40a7b8fc5ce334ef8a75e673fdd8614ee6575a
dwayhs <danielwayhs@gmail.com>
2017-01-04 22:56:34 -0200

    doc: Annotating event model

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  when        :datetime
+#  description :text
+#  where       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  name        :string
+#
+
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
